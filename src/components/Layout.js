fdf8d04cda4e7e7069ba211fce3792e76195e60a
sibongiseni <sibongiseni.ndebele@idolconsulting.co.za>
2021-05-18 10:28:02 +0200

    project & sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,16 @@ import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [isOPen, setIsOPen] = React.useState(false);
+  const toggleSidebar = () => {
+    setIsOPen(!isOPen)
+  }
   return (
     <>
-      <Navbar/>
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOPen={isOPen} toggleSidebar={toggleSidebar} />
       {children}
+      <Footer />
     </>
   )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,18 @@ import React from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
-const Sidebar = () => {
-  return <h2>sidebar component</h2>
+const Sidebar = ({isOPen, toggleSidebar}) => {
+  return (
+    <aside className={`sidebar ${isOPen ? "show-sidebar" : " "}`}>
+      <button className="close-btn" onClick={toggleSidebar}>
+        <FaTimes />
+      </button>
+      <div className="side-container">
+        <Links styleClass={`${isOPen ? "sidebar-links" : ""}`} />
+        <SocialLinks styleClass={`${isOPen ? "sidebar-icons" : ""}`}  />
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
